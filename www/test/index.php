081609e354e3cb5da2553f21066122f8bd5c86ac
lihq1403 <lihaiqing1994@163.com>
2018-09-09 16:20:20 +0800

    mysql挂载没解决

--- www/test/index.php
+++ www/test/index.php
@@ -1,2 +1,10 @@
 <?php
+
+try {
+	$pdo = new PDO('mysql:dbname=test;host=mysql5.6.40;charset=utf8mb4;port=3306','root','root');
+} catch (Exception $e) {
+	echo $e->getMessage();
+}
+
+var_dump($pdo);exit();
  phpinfo();
